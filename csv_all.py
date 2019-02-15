0d1f0df4b516c3c8abbbd1e11a42816757601955
Der Weissmeister <weiss@meister.de>
2019-02-15 13:55:11 +0100

    Cleaned up some code in the csv scripts, as well as adding tqdm status bars

--- csv_all.py
+++ csv_all.py
@@ -1,5 +1,6 @@
 from os import listdir
 from config import *
+from tqdm import tqdm
 
 
 def addtoarr(masterarr, filename):  # Creates array with the names of all the people.
@@ -11,7 +12,7 @@ def addtoarr(masterarr, filename):  # Creates array with the names of all the pe
     return masterarr
 
 
-def getfilelist():
+def getfilelist():  # Gets a list of all .list files. (Files that contain friend information)
     filelist = listdir()
     fixedfilelist = []
     for file3 in filelist:
@@ -23,24 +24,19 @@ def getfilelist():
 # Get file list and make the array of all people
 file_list = getfilelist()
 people_list = ["nothing", config_name]
-for file in file_list:
+
+print("Processing listfiles, this can take a while...")
+for file in tqdm(file_list):
     addtoarr(people_list, file)
-    print("Processed {0}".format(file))
 print("All listfiles processsed!")
 
-# Export nodes.csv
-csv_str_list = []
-for (num, name) in enumerate(people_list):
-    if num != 0:
-        composed = '{0},"{1}"\n'.format(str(num), name)
-        csv_str_list.append(composed)
-
+# Save nodes.csv
 with open("nodes.csv", "w") as file:
-    file.write("ID,Label")
-    for composedstr in csv_str_list:
-        file.write(composedstr)
-
-print("csv exported!")
+    file.write("ID,Label\n")
+    for (num,name) in enumerate(people_list):
+        if num != 0:
+            file.write('{0},"{1}"\n'.format(str(num), name))
+print("nodes.csv exported!")
 
 # Put all people in a dictionary
 people_dict = {}
@@ -48,15 +44,16 @@ for i in range(len(people_list)):
     people_dict[people_list[i]] = i
 
 # Save edges.csv
+print("Exporting edges.csv...")
 with open("edges.csv", "w") as edges:
     edges.write("Source,Target\n")
-    for filename in file_list:
-        master_name = filename[:-5]
-        master_int = people_dict[master_name]
+    for filename in tqdm(file_list):
+        master_name = filename[:-5]  # Split off the .list, gives you the full name.
+        master_int = people_dict[master_name]  # Gets the integer belonging to that name, needed for the csv
         with open(filename, "r") as file:
             file_lines = file.readlines()
             for line in file_lines:
                 slave_name = line[:-1]  # Split off \n
                 slave_int = people_dict[slave_name]
                 edges.write("{0},{1}\n".format(str(master_int), str(slave_int)))
-        print("{0} Done!".format(filename))
+print("edges.csv exported!")
--- csv_friends.py
+++ csv_friends.py
@@ -1,16 +1,21 @@
 from os import listdir
 from config import *
+from tqdm import tqdm
 
 
-def getnamelist():
+def getnamelist():  # Gets a list of the names of all the people involved, including config_name.
     namelist = ["nothing", config_name]
-    with open("{0}.list".format(config_name), "r") as file4:
-        for line2 in file4:
-            namelist.append(line2[:-1])
-    return namelist
+    try:
+        with open("{0}.list".format(config_name), "r") as file4:
+            for line2 in file4:
+                namelist.append(line2[:-1])
+        return namelist
+    except IOError:
+        print("Error: {0}.list not found or you do not have read permission.\nTerminating...".format(config_name))
+        exit()
 
 
-def getfilelist():
+def getfilelist():  # Gets a list of all .list files. (Files that contain friend information)
     filelist = listdir()
     fixedfilelist = []
     for file3 in filelist:
@@ -23,18 +28,13 @@ def getfilelist():
 file_list = getfilelist()
 people_list = getnamelist()
 
-# Export first csv file
-csv_str_list = []
-for (num, name) in enumerate(people_list):
-    if num != 0:
-        composed = '{0},"{1}"\n'.format(str(num), name)
-        csv_str_list.append(composed)
-
+# Save nodes.csv
 with open("nodes.csv", "w") as file:
     file.write("ID,Label\n")
-    for composedstr in csv_str_list:
-        file.write(composedstr)
-print("csv exported!")
+    for (num,name) in enumerate(people_list):
+        if num != 0:
+            file.write('{0},"{1}"\n'.format(str(num), name))
+print("nodes.csv exported!")
 
 # Put all people in a dictionary
 people_dict = {}
@@ -42,16 +42,17 @@ for i in range(len(people_list)):
     people_dict[people_list[i]] = i
 
 # Save edges.csv
+print("Exporting edges.csv...")
 with open("edges.csv", "w") as edges:
     edges.write("Source,Target\n")
-    for filename in file_list:
-        master_name = filename[:-5]
-        master_int = people_dict[master_name]
-        with open(filename, "r") as file:
+    for filename in tqdm(file_list):
+        master_name = filename[:-5]  # Split off the .list, gives you the full name.
+        master_int = people_dict[master_name]  # Gets the integer belonging to that name, needed for the csv.
+        with open(filename, "r") as file:  # no IO ex-catching needed since we retrieved the filelist 5 seconds ago.
             file_lines = file.readlines()
             for line in file_lines:
                 slave_name = line[:-1]  # Split off \n
                 if slave_name in people_list:
                     slave_int = people_dict[slave_name]
                     edges.write("{0},{1}\n".format(str(master_int), str(slave_int)))
-        print("{0} Done!".format(filename))
+print("edges.csv exported!")
